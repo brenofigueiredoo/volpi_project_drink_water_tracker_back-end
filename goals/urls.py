27553c468dd808168bfe5be0689565810399c157
unknown <brenos93@gmail.com>
2023-08-03 15:27:54 -0300

    fix: corrigindo endereco de rota incorreta

--- goals/urls.py
+++ goals/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("goals/user/<uuid:user_id>", views.GoalCreateView.as_view()),
-    path("goals/user/<uuid:user_id>", views.GoalsUserListView.as_view()),
+    path("goals/user/create/<uuid:user_id>", views.GoalCreateView.as_view()),
+    path("goals/user/list/<uuid:user_id>", views.GoalsUserListView.as_view()),
     path("goals/<uuid:goal_id>", views.GoalDetailView.as_view()),
     path("goals/<uuid:goal_id>/drinkwater", views.GoalUpdateView.as_view()),
 ]
