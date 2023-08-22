7a56df11abb2d4d9aa72e78215ee5cf7a1910971
unknown <brenos93@gmail.com>
2023-08-22 17:41:56 -0300

    feat: Adicionando rota que lista todos os usuários
    No retorno da rota, mostra a meta atual e a quantidade de metas diárias / metas atigindas. E por fim, retorna um objeto contendo a média total de metas entre os usuários.

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User
+from goals.models import Goals
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,3 +15,27 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = [
             "goal_ml",
         ]
+
+
+class ListAllUsersSerializer(serializers.ModelSerializer):
+    completed_goals = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "name",
+            "weight_kg",
+            "goal_ml",
+            "completed_goals",
+        ]
+
+    def get_completed_goals(self, obj: User):
+        quantity_of_goals = len(Goals.objects.filter(user_id=obj.id))
+        completed_goals = 0
+
+        for goals in Goals.objects.filter(user_id=obj.id):
+            if goals.goal_consumed_percentage == 100:
+                completed_goals += 1
+
+        return f"{completed_goals}/{quantity_of_goals}"
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework.response import Response
+from .serializers import UserSerializer, ListAllUsersSerializer
 from .models import User
 
 
-class UserView(generics.CreateAPIView):
+class UserView(generics.ListCreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
@@ -12,6 +13,25 @@ class UserView(generics.CreateAPIView):
         goal_ml = round(weight_kg * 35, 2)
 
         serializer.save(goal_ml=goal_ml)
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return ListAllUsersSerializer
+        return UserSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        total_goals = sum(user.goal_ml for user in queryset)
+        total_average_of_goals = round(total_goals / len(queryset), 2)
+
+        serializer = self.get_serializer(queryset, many=True)
+        serialized_data = serializer.data
+
+        custom_object = {"total_average_of_goals": total_average_of_goals}
+        serialized_data.append(custom_object)
+
+        return Response(serialized_data)
 
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
